2505eba7345cea46eabd116c4df3b634e82dbd82
madan-ram <madan_ram@rocketmail.com>
2015-10-30 12:49:32 +0530

    remove unwanted files

--- segment.cpp
+++ segment.cpp
@@ -20,7 +20,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 #include <cstdlib>
 #include <image.h>
 #include <misc.h>
-#include <pnmfile.h>
 #include <imagefile.h>
 #include "segment-image.h"
 
