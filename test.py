225e08ad9f8a346439275ba364bd78ab08d7589a
madan-ram <madan_ram@rocketmail.com>
2015-11-02 12:48:38 +0530

    moved file to segment and created module

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 import cv2
+import time
+import numpy
 import segment
-img = cv2.imread('input/0.jpg')
-print (segment.get_bounded_box(img, 0.5, 1000, 10))
+
+img = cv2.imread('input/1.jpg')
+start_time = time.time()
+print len(segment.segment.get_bounded_box(img, 0.5, 500, 20))
+end_time = time.time()
+print end_time - start_time, 'time spent'
